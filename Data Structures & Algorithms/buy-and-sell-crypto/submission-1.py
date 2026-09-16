80328a03fa00dc1740d64b17272307a394b5d3ba
conroy96 <eoin.conroy2@ucdconnect.ie>
2026-09-16 20:19:59 +0100

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <1:
+            return 0
+        lowest= prices[0]
+        profit=0
+        for i in range(len(prices)):
+            if prices[i] <lowest:
+                lowest= prices[i]
+            elif prices[i] - lowest > profit:
+                profit = prices[i] -lowest
+        return profit
